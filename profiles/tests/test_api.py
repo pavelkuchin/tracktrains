0741150668f72845c7f6461c621f7c393f5dae74
Pavel Kuchin <pavel.s.kuchin@gmail.com>
2014-11-01 21:58:51 +0300

    Additional unittests for profiles api.

--- profiles/tests/test_api.py
+++ profiles/tests/test_api.py
@@ -18,6 +18,9 @@ class TestTrackTrainsUserResource(ResourceTestCase):
         self.user_email = u"test@test.ts"
         self.user_pass = u"test"
 
+        self.userwi_email = u"testwi@test.ts"
+        self.userwi_pass = u"test"
+
         self.list_url = u"/api/v1/user/"
         self.details_url = u"/api/v1/user/%d/"
         self.invite_url = u"/api/v1/user/invite/%s/"
@@ -30,6 +33,11 @@ class TestTrackTrainsUserResource(ResourceTestCase):
         self.user = TrackTrainsUser.objects.\
             create_user(self.user_email, self.super_user_email, self.user_pass)
 
+        self.user_without_invitation = TrackTrainsUser.objects.\
+            create_user(self.userwi_email, self.super_user_email, self.userwi_pass)
+        self.user_without_invitation.invites_counter=0
+        self.user_without_invitation.save()
+
         self.post_data = {
             'email': 'newtest@test.ts',
             'inviter': self.details_url % (self.superuser.id),
@@ -37,8 +45,8 @@ class TestTrackTrainsUserResource(ResourceTestCase):
             'password': 'test'
         }
 
-    def get_credentials(self, superuser=False):
-        if superuser:
+    def get_credentials(self, without_invitations=False):
+        if without_invitations:
             return self.create_basic(
                 username=self.super_user_email,
                 password=self.super_user_pass)
@@ -59,7 +67,7 @@ class TestTrackTrainsUserResource(ResourceTestCase):
             authentication=self.get_credentials())
         self.assertValidJSONResponse(resp)
 
-        self.assertEqual(len(self.deserialize(resp)['objects']), 2)
+        self.assertEqual(len(self.deserialize(resp)['objects']), 3)
 
         self.assertEqual(self.deserialize(resp)['objects'][1], {
             u'resource_uri': self.details_url % (self.user.pk),
@@ -203,3 +211,74 @@ class TestTrackTrainsUserResource(ResourceTestCase):
         self.assertIsNotNone(user)
         self.assertEqual(user.email, email)
         self.assertEqual(user.is_active, True)
+
+    def test_bad_invitation(self):
+        token = "bad token"
+        password = "testpassword"
+
+        # registartion with token
+        resp = self.deserialize(
+            self.api_client.post(
+                self.signup_url % (token),
+                format='json',
+                data={'password': password}
+            )
+        )
+
+        self.assertIsNotNone(resp)
+        self.assertEqual(resp["success"], False)
+
+    def test_bad_password(self):
+        email = "unittest@test.ts"
+        password = ""
+        pattern = r"/signup/(.+)/"
+
+        # send invitation
+        resp = self.deserialize(
+            self.api_client.post(
+                self.invite_url % (email),
+                authentication=self.get_credentials())
+        )
+
+        self.assertIsNotNone(resp)
+        self.assertEqual(resp["success"], True, resp["message"])
+        # checking email and finding token in email
+        # The outbox attribute is created when the first message is sent.
+        self.assertIsNotNone(mail.outbox)
+        self.assertEqual(len(mail.outbox), 1)
+
+        msg = mail.outbox[0]
+
+        sres = re.search(pattern, msg.body)
+
+        self.assertIsNotNone(sres)
+
+        token = sres.group(1)
+
+        self.assertIsNotNone(token)
+        self.assertGreater(len(token), 0)
+
+        # registartion with token
+        resp = self.deserialize(
+            self.api_client.post(
+                self.signup_url % (token),
+                format='json',
+                data={'password': password}
+            )
+        )
+
+        self.assertIsNotNone(resp)
+        self.assertEqual(resp["success"], False)
+
+    def test_without_invitations(self):
+        email = "unittest@test.ts"
+
+        # send invitation
+        resp = self.deserialize(
+            self.api_client.post(
+                self.invite_url % (email),
+                authentication=self.get_credentials(True))
+        )
+
+        self.assertIsNotNone(resp)
+        self.assertEqual(resp["success"], False)
