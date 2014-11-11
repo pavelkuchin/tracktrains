3bbb0a9be45cbec8ce8cd81aadeb7c4167deae55
Pavel Kuchin <pavel.s.kuchin@gmail.com>
2014-11-11 20:39:03 +0300

    Additional secure settings for prod env, and logging.

--- tracktrains/settings/prod.py
+++ tracktrains/settings/prod.py
@@ -7,6 +7,9 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 TASTYPIE_FULL_DEBUG = DEBUG
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE¶ = True
+
 ADMINS = (('Pavel Kuchin', 'pavel.s.kuchin@gmail.com'),)
 MANAGERS = ADMINS
 
@@ -15,4 +18,33 @@ ALLOWED_HOSTS = [
     '.%s' % HOST
 ]
 
-LOGGING = {}
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+      'basic': {
+        'format': '%(levelname)s %(asctime)s %(message)s'
+      }
+    },
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': get_env_var('APP_ROOT') + '/logs/tracktrains.log',
+            'maxBytes': 1024*1024*5, # 5 MB
+            'backupCount': 5,
+            'formatter': 'basic'
+        },
+    },
+    'loggers': {
+        '': {
+            'level': 'WARNING',
+            'handlers': ['file']
+        },
+        'django': {
+            'level': 'WARNING',
+            'handlers': ['file'],
+            'propagate': False,
+        }
+    }
+}
