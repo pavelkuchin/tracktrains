5503ee90b7557bc68e49bf0013402c5470131208
Pavel Kuchin <pavel.s.kuchin@gmail.com>
2015-04-21 15:54:48 +0300

    Additional security options for Django.

--- tracktrains/settings/base.py
+++ tracktrains/settings/base.py
@@ -68,6 +68,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware'
 )
 
 ROOT_URLCONF = 'tracktrains.urls'
